3557bb28da633c9cf9a048c3c0f6b3930ebc7ad3
Artyom Lozovski <artik4444@outlook.com>
2020-06-05 20:20:47 +0300

    Add project files.

---
+++ NearApp/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NearApp.Models;
+
+namespace NearApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize(Roles = "admin, user")]
+        public IActionResult Index()
+        {
+            string role = User.FindFirst(x => x.Type == ClaimsIdentity.DefaultRoleClaimType).Value;
+            return Content($"ваша роль: {role}");
+        }
+        [Authorize(Roles = "admin")]
+        public IActionResult About()
+        {
+            return Content("Вход только для администратора");
+        }
+    }
+}
